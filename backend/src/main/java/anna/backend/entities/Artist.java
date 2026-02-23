1c1c3c7de5d6ec4e70390b43f2086d0a2217781a
kecand22 <kecand22@htl-kaindorf.at>
2026-02-23 08:52:01 +0100

    Add properties and annotations for class Artist.java

--- backend/src/main/java/anna/backend/entities/Artist.java
+++ backend/src/main/java/anna/backend/entities/Artist.java
@@ -1,10 +1,20 @@
 package anna.backend.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
 @Entity
 public class Artist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long artistId;
+    private String firstname;
+    private String lastname;
+    private String description;
+    private String imageUrl;
 
 }
