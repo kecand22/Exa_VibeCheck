9a8e7ee145b9561b51bc6262bcf35bf7e4babd0c
kecand22 <kecand22@htl-kaindorf.at>
2026-02-23 08:47:25 +0100

    Add properties and annotations in class Rating.java

--- backend/src/main/java/anna/backend/entities/Rating.java
+++ backend/src/main/java/anna/backend/entities/Rating.java
@@ -1,9 +1,20 @@
 package anna.backend.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Data
 public class Rating {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long ratingId;
+    private Integer stars;
+    private String comment;
+    private LocalDateTime createdAt;
 }
