604e89913c1f8e7aa1aa535f823ea11ae6c8a04a
kecand22 <kecand22@htl-kaindorf.at>
2026-02-23 08:54:53 +0100

    Add properties and annotations for class Event.java

--- backend/src/main/java/anna/backend/entities/Event.java
+++ backend/src/main/java/anna/backend/entities/Event.java
@@ -1,10 +1,22 @@
 package anna.backend.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 @Entity
 public class Event {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long eventId;
+    private String title;
+    private String location;
+    private LocalDate eventDate;
+    private String imageUrl;
 }
