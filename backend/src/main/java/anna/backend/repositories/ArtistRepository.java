0d1619439d36eba7af972a0e28f6b825a3d3ecdc
kecand22 <kecand22@htl-kaindorf.at>
2026-03-03 11:33:23 +0100

    Create Repositories

---
+++ backend/src/main/java/anna/backend/repositories/ArtistRepository.java
@@ -0,0 +1,12 @@
+package anna.backend.repositories;
+
+import anna.backend.entities.Artist;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ArtistRepository extends JpaRepository<Artist, Long>{
+    Optional<Artist> getAllArtists();
+}
