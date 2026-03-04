506d9601ae5791c2bf81add6d02a8288b6e2d60f
kecand22 <kecand22@htl-kaindorf.at>
2026-03-04 09:59:45 +0100

    Add findAllByEvent_EventId to RatingRepository.java

--- backend/src/main/java/anna/backend/repositories/RatingRepository.java
+++ backend/src/main/java/anna/backend/repositories/RatingRepository.java
@@ -4,6 +4,10 @@ import anna.backend.entities.Rating;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface RatingRepository extends JpaRepository<Rating, Long> {
+    List<Rating> findAllByEvent_EventId(Long eventEventId);
 }
