dab7d9d389d118249d53e77773f346083964db17
kecand22 <kecand22@htl-kaindorf.at>
2026-03-04 09:47:49 +0100

    Add getArtistById to ArtistRepository.java

--- backend/src/main/java/anna/backend/repositories/ArtistRepository.java
+++ backend/src/main/java/anna/backend/repositories/ArtistRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface ArtistRepository extends JpaRepository<Artist, Long>{
-    Optional<Artist> getAllArtists();
+    Optional<Artist> getArtistsByArtistId(Long artistId);
 }
