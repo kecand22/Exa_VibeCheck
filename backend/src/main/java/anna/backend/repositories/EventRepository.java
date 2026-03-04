992068a776fd797384a6e1fd7598df62c3d8881c
kecand22 <kecand22@htl-kaindorf.at>
2026-03-04 11:04:19 +0100

    Implement the findEventsByArtistId in EventRepository.java

--- backend/src/main/java/anna/backend/repositories/EventRepository.java
+++ backend/src/main/java/anna/backend/repositories/EventRepository.java
@@ -1,9 +1,20 @@
 package anna.backend.repositories;
 
 import anna.backend.entities.Event;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
+    @Query("""
+            SELECT DISTINCT e 
+            FROM Event e 
+            INNER JOIN e.artists a            
+            WHERE a.artistId = :artistId      """)
+
+    List<Event> findEventsByArtistId(Long artistId);
 }
