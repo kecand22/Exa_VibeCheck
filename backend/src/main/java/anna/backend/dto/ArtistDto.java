160055137f4ac01606f66cfbf0082c519ae3c3ec
kecand22 <kecand22@htl-kaindorf.at>
2026-03-06 19:32:27 +0100

    add an ArtistDto

--- backend/src/main/java/anna/backend/dto/ArtistDto.java
+++ backend/src/main/java/anna/backend/dto/ArtistDto.java
@@ -1,4 +1,10 @@
 package anna.backend.dto;
 
 public class ArtistDto {
+    private Long artistId;
+
+    private String firstName;
+    private String lastName;
+    private String description;
+    private String imageUrl;
 }
