5c5a5f08dc554f1e5b9f9727a22c7dca9f2aac01
kecand22 <kecand22@htl-kaindorf.at>
2026-03-07 00:39:51 +0100

    change EventCard.tsx

---
+++ frontend/src/components/EventCard.tsx
@@ -0,0 +1,48 @@
+import React, {useEffect, useState} from 'react';
+import {Card, CardContent, CardMedia, Rating, Typography} from "@mui/material";
+import type {EventModel} from "../models/EventModel.ts";
+import {useNavigate} from "react-router";
+
+export interface EventCardProps {
+    event: EventModel
+}
+
+
+const EventCard: React.FC<EventCardProps> = ({event}) => {
+    const navigate = useNavigate();
+    const [average, setAverage] = useState<number>(0.0);
+
+    useEffect(() => {
+        console.log(typeof event.eventDate);
+
+        let sum = 0;
+        event.ratings.forEach(r => sum += r.stars);
+
+        setAverage(sum / event.ratings.length);
+    }, [event]);
+
+
+    return (
+        <>
+            <Card sx={{ maxWidth: 345 }}
+                  onClick={() => navigate(`/events/${event.eventId}/`)}>
+                <CardMedia
+                    sx={{ height: 140 }}
+                    image={event.imageUrl}
+                    title="artist"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {event.title}
+                    </Typography>
+                    <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                        {event.location} {event.eventDate.toLocaleDateString()}
+                    </Typography>
+                    <Rating name="read-only" value={average} readOnly />
+                </CardContent>
+            </Card>
+        </>
+    );
+};
+
+export default EventCard;
