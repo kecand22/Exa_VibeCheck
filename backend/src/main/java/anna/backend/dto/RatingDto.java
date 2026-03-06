957f26e8c12a8f762864772401269587c202ee50
kecand22 <kecand22@htl-kaindorf.at>
2026-03-06 19:34:45 +0100

    add an RatingDto

--- backend/src/main/java/anna/backend/dto/RatingDto.java
+++ backend/src/main/java/anna/backend/dto/RatingDto.java
@@ -1,4 +1,15 @@
 package anna.backend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
 public class RatingDto {
+    private Long id;
+    private Integer stars;
+    private String comment;
+    private LocalDateTime createdAt;
 }
